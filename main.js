58f0f04d302ccb807223a903ba7929a84a53955c
Ysnaldo Lopez <ysnaldolopez@gmail.com>
2021-04-25 17:31:24 -0500

    primeros cambios

---
+++ main.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// var title = document.querySelector('.main-title').innerHTML = 'Hola Mundo';
+
+// const listElement = document.querySelector('.list');
+// const element = `
+//     <li>
+//         <a href = "#">Home</a>
+//     </li>
+//     <li>
+//         <a href = "#">Contactos</a>
+//     </li>
+//     <li>
+//         <a href = "#">Descubrenos</a>
+//     </li>
+// `;
+
+// listElement.innerHTML = element; 
+
+// const name = 'Ana';
+// const surname = 'LoveGame';
+
+// console.log(`My name is ${name} ${surname}`)
+
+// var nombres = ['Ana', 'Gabriel', 'Carlos'];
+// let tabla = document.getElementById("table");
+
+// nombres.forEach((personas) => {
+//     let elementos = `
+//     <tr>
+//         <td> Hola soy ${personas} </td> 
+//     </tr>
+//     `;
+//     tabla.innerHTML += elementos;
+// });
+
+// Logo pequeño 
+
+// const small_Logo = document.querySelector('.logo-pequeño');
+// const small_logoContainer = document.createElement('div');
+// small_logoContainer.innerHTML = `
+//     <h1>Es una prueba</h1>
+
+// `;
