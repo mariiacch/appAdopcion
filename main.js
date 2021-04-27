8844b82cd6517b8f13df88a29fe6a6901e1cb42f
Ysnaldo Lopez <ysnaldolopez@gmail.com>
2021-04-27 15:16:20 -0500

    Cambios martes

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
-'use strict';
+// 'use strict';
 
-setInterval(function(){
-    cargaPagina();
-}, 2000);
+// setInterval(function(){
+//     cargaPagina();
+// }, 2000);
 
-function cargaPagina(){
-    document.getElementById('loader').classList.add('hide');
+// function cargaPagina(){
+//     document.getElementById('loader').classList.add('hide');
+// }
+
+// Funcion para quitar secciones A-B 
+function quitarSeccion(){
+    const section_uno = document.querySelector('.container_section_uno');
+    section_uno.classList.add('hide');
+    const section_dos = document.querySelector('.container_section_dos');
+    section_dos.classList.remove('hide');
 }
 
 
+// Evento de boton siguiente
+const activarSeccionB = document.getElementById('siguiente_uno');
+activarSeccionB.addEventListener("click", quitarSeccion);
 
